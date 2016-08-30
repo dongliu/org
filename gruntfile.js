c4da25f1846bf7be3f1489b4c0afc2e26a9b89eb
Dong Liu <edongliu@gmail.com>
2016-08-30 16:57:06 -0400

    add grunt file

---
+++ gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function (grunt) {
+
+  require('load-grunt-tasks')(grunt);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      target: ['Gruntfile.js', 'lib/**/*.js', 'routes/**/*/js', 'public/javascripts/*.js', 'test/**/*.js']
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    shell: {
+      options: {
+        stderr: false
+      },
+      puglint: {
+        command: './node_modules/pug-lint/bin/pug-lint ./views/*.pug ./views/client-side/*.pug'
+      }
+    }
+  });
+
+  grunt.registerTask('test', ['mochaTest']);
+  grunt.registerTask('puglint', ['shell:puglint']);
+  grunt.registerTask('eslint', ['eslint']);
+  grunt.registerTask('default', ['puglint', 'eslint']);
+
+};
